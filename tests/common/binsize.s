a95b9793a2206d5cf685b2ccec7120f39438a551
Sandor Zsuga <jubatian@jubatian.com>
2017-07-27 14:42:51 +0200

    Updated CRC component tests to generate binaries with CRC added.

---
+++ tests/common/binsize.s
@@ -0,0 +1,39 @@
+;
+; XMBurner test - Binary size
+;
+; Copyright (C) 2017 Sandor Zsuga (Jubatian)
+;
+;  This program is free software: you can redistribute it and/or modify
+;  it under the terms of the GNU General Public License as published by
+;  the Free Software Foundation, either version 3 of the License, or
+;  (at your option) any later version.
+;
+;  This program is distributed in the hope that it will be useful,
+;  but WITHOUT ANY WARRANTY; without even the implied warranty of
+;  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;  GNU General Public License for more details.
+;
+;  You should have received a copy of the GNU General Public License
+;  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+;
+
+
+#include <avr/io.h>
+
+
+.section .text
+
+
+#ifdef BINSIZE
+
+;
+; This provides the binary size for components needing it.
+;
+.global xmb_bsize
+xmb_bsize:
+	.byte (BINSIZE      ) & 0xFF
+	.byte (BINSIZE >>  8) & 0xFF
+	.byte (BINSIZE >> 16) & 0xFF
+	.byte (BINSIZE >> 24) & 0xFF
+
+#endif
