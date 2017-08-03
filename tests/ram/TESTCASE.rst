613aa2d91ea6bfa3d8a0806bcb45aa0868a18ac5
Sandor Zsuga <jubatian@jubatian.com>
2017-08-03 13:52:26 +0200

    Added RAM stuck 0/1 bit tests.

--- tests/ram/TESTCASE.rst
+++ tests/ram/TESTCASE.rst
@@ -36,3 +36,24 @@ Tests whether the module can run properly when there are no anomalies in the
 ALU. This means that the module returns without calling the fault handler.
 
 No ALU modifications are present.
+
+
+
+Case 20 - 27
+------------------------------------------------------------------------------
+
+
+Tests stuck to 0 bits in RAM on the RAM check (xmb_ram_check). It must detect
+any stuck 0 bit.
+
+ALU modifications: Each case tests a different stuck to 0 bit on a RAM cell.
+
+
+Case 30 - 37
+------------------------------------------------------------------------------
+
+
+Tests stuck to 1 bits in RAM on the RAM check (xmb_ram_check). It must detect
+any stuck 1 bit.
+
+ALU modifications: Each case tests a different stuck to 1 bit on a RAM cell.
---
+++ tests/ram/ram__23.s
@@ -0,0 +1,90 @@
+;
+; XMBurner test - xmb_ram.s, case 23
+;
+; Copyright (C) 2017 Sandor Zsuga (Jubatian)
+;
+;  This program is free software: you can redistribute it and/or modify
+;  it under the terms of the GNU General Public License as published by
+;  the Free Software Foundation, either version 3 of the License, or
+;  (at your option) any later version.
+;
+;  This program is distributed in the hope that it will be useful,
+;  but WITHOUT ANY WARRANTY; without even the implied warranty of
+;  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;  GNU General Public License for more details.
+;
+;  You should have received a copy of the GNU General Public License
+;  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+;
+;
+; Tests RAM location bit 3 stuck 0 on xmb_ram_check
+;
+
+
+#include <avr/io.h>
+.include "ram_inc.inc"
+
+
+.section .text
+
+
+.global main
+main:
+
+	; Output test ID
+
+	PRINT_TEST_ID  23
+
+	; Prepare for test
+
+	call  xmb_ram_init
+	ldi   XL,      lo8((RAMEND + 1 - RAMSTART) / 16)
+	ldi   XH,      hi8((RAMEND + 1 - RAMSTART) / 16)
+	MOD_STUCK_R16  0x00, 0xF7, RAMSTART + 53 ; Stuck 0 bit
+	MOD_ENA_R16
+
+	; Run test
+
+main_l:
+	push  XL
+	push  XH
+	LD_FPTR_R24_25 xmb_ram_check
+	call  runner_run
+	pop   XH
+	pop   XL
+	sbiw  XL,      1
+	brne  main_l
+
+	; If routine completes, then fails
+
+	jmp   print_fail_nodet
+
+
+
+;
+; XMBurner fault handler
+;
+.global xmb_fault
+xmb_fault:
+
+	MOD_DIS_R16
+
+	; Evaulate
+
+	cpi   r24,     0x01
+	brne  fail             ; Must be 0x01
+	cpi   r25,     0x04
+	brne  fail
+
+	; Passed test
+
+	jmp   print_pass
+
+
+
+;
+; Failed test
+;
+fail:
+
+	jmp   print_fail_val
